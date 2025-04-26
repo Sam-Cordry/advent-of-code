53ffeb86a5cc326ddcee3666a066b59ced91f88f
Sam Cordry <samcordry@gmail.com>
2025-04-25 21:59:22 -0400

    2015: add day 6

---
+++ 2015/day6/src/main.rs
@@ -0,0 +1,106 @@
+fn main() {
+    println!("Part 1: {}", part1());
+    println!("Part 2: {}", part2());
+}
+
+fn part1() -> usize {
+    let mut lights: Vec<Vec<bool>> = vec![vec![false; 1000]; 1000];
+
+    for l in std::fs::read_to_string("input")
+        .unwrap()
+        .lines()
+        .map(|l| l.trim())
+    {
+        let instr = l.split_whitespace().collect::<Vec<_>>();
+
+        if instr.len() == 4 {
+            let start: Vec<usize> = instr[1]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+            let end: Vec<usize> = instr[3]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+
+            for row in lights.iter_mut().take(end[1] + 1).skip(start[1]) {
+                for light in row.iter_mut().take(end[0] + 1).skip(start[0]) {
+                    *light = !*light;
+                }
+            }
+        } else {
+            let start: Vec<usize> = instr[2]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+            let end: Vec<usize> = instr[4]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+
+            for row in lights.iter_mut().take(end[1] + 1).skip(start[1]) {
+                for light in row.iter_mut().take(end[0] + 1).skip(start[0]) {
+                    *light = instr[1] == "on";
+                }
+            }
+        }
+    }
+
+    lights
+        .into_iter()
+        .map(|r| r.into_iter().filter(|v| *v).count())
+        .sum()
+}
+
+fn part2() -> usize {
+    let mut lights: Vec<Vec<usize>> = vec![vec![0; 1000]; 1000];
+
+    for l in std::fs::read_to_string("input")
+        .unwrap()
+        .lines()
+        .map(|l| l.trim())
+    {
+        let instr = l.split_whitespace().collect::<Vec<_>>();
+
+        if instr.len() == 4 {
+            let start: Vec<usize> = instr[1]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+            let end: Vec<usize> = instr[3]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+
+            for row in lights.iter_mut().take(end[1] + 1).skip(start[1]) {
+                for light in row.iter_mut().take(end[0] + 1).skip(start[0]) {
+                    *light += 2;
+                }
+            }
+        } else {
+            let start: Vec<usize> = instr[2]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+            let end: Vec<usize> = instr[4]
+                .split(",")
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect();
+
+            for row in lights.iter_mut().take(end[1] + 1).skip(start[1]) {
+                for light in row.iter_mut().take(end[0] + 1).skip(start[0]) {
+                    if instr[1] == "on" {
+                        *light += 1;
+                    } else if *light > 0 {
+                        *light -= 1;
+                    }
+                }
+            }
+        }
+    }
+
+    lights
+        .into_iter()
+        .map(|r| r.into_iter().sum::<usize>())
+        .sum()
+}
